eabd91b27458efca1e8985768dd45d2d254f86de
torri-bra <torri-bra@gmx.com>
2025-03-04 18:31:38 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,10 @@
+// Random Java Code
+
+import java.util.Random;
+
+public class RandomCode {
+  public static void main(String[] args) {
+    int randomNumber = new Random().nextInt(10);
+    System.out.println("The random number is: " + randomNumber);
+  }
+}
