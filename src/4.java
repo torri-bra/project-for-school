bdeaa4ba961a6207041031ecd4db2068fdfe8bc5
torri-bra <torri-bra@gmx.com>
2025-03-06 18:50:28 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class RandomJavaCode {
+    public static void main(String[] args) {
+        int x = 42;
+        String y = "Hello world!";
+        System.out.println("The value of x is: " + x);
+        System.out.println("The value of y is: " + y);
+    }
+}
